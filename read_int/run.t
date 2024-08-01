0f84cd1d2b5e74304c4e839b17c822d9bbe40062
Kakadu <Kakadu@pm.me>
2024-08-01 17:29:48 +0300

    Reading integer works
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- read_int/run.t
+++ read_int/run.t
@@ -6,3 +6,7 @@ $ export QEMU_STRACE=1
 $ export QEMU_STRACE=1
   $ bash -c "echo -e '5678\n' | qemu-riscv64 -L /usr/riscv64-linux-gnu  ./rv64.exe"
                               5678
+
+  $ bash -c "echo -e '0123\n' | qemu-riscv64 -L /usr/riscv64-linux-gnu  ./rv64.exe"
+Error test
+  $ bash -c "echo -e 'x\n' | qemu-riscv64 -L /usr/riscv64-linux-gnu  ./rv64.exe"
--- read_int/rv64.s
+++ read_int/rv64.s
@@ -1,29 +1,32 @@
 
 .section .data
-demostring1:    .string "01234567012345670123456701234567\n" # 32 bytes
-demostring2:    .string "01234\67012345670123456701234567\n" # 32 bytes
 tempread: .string "x"
 
-.text
-.global _start # Provide program starting address to linker
+.section .text
+.global _start
 _start:
 .option push # MAGIC
 .option norelax
   la gp, __global_pointer$
 .option pop
   call read_int
+  bne a1, zero, .main_error
   call print_int
 
-
-  addi a0, x0, 0   # Use 0 return code
-  #addi    a7, x0, 93  # Service command code 93 terminates
-  li  a7, 93 # Service command code 93 terminates
+  li a0, 0
+  li a7, 93 # Service command code 93 terminates
   ecall      # Call linux to terminate the program
+.main_error:
+  li a0, 1
+  li a7, 93 # Service command code 93 terminates
+  ecall
 
 .global read_int
 read_int:
+  # returns result in a0
+  # if a1 <> 0, it is an error
   la a6, tempread
-  li t3, 0
+  li t3, 0  # accum
 .read_int_loop:
   li a7, 63 # read
   li a0, 0
@@ -31,17 +34,22 @@ read_int:
   li a2, 1
   ecall
   lb t0, (a6)
-  li a5, 47
-  ble t0, a5, .read_int_exit
+  li a5, 10 # ascii \n
+  beq t0, a5, .read_int_exit
+  li a5, 48
+  blt t0, a5, .read_int_exit_error
   li a5, 58
-  ble a5, t0, .read_int_exit
+  bge t0, a5, .read_int_exit_error
   addi t0, t0, -48
   li t4, 10
   mul t3, t3, t4
   add t3, t3, t0
   j .read_int_loop
 .read_int_exit_error:
-  mv a0, t3
+  li a0, 0
+  li a1, 1
+  ret
 .read_int_exit:
+  li a1, 0
   mv a0, t3
   ret
